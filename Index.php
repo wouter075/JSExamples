36351a08ab0320f608c0fa97d3d73c1408b9e48a
Gregory <Gregory@gregory.nl>
2019-02-04 13:50:35 +0100

    Initial commit

---
+++ Index.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</HEAD>
+<BODY>
+
+
+<div id="box1" class="box"></div>
+<div id="box2" class="box"></div>
+
+
+</BODY>
+</HTML>
